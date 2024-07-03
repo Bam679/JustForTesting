706b6c4748b9dfa9b5b62f4a4f9456174b333389
Bam679 <michael.bachmann@perinova.com>
2024-07-03 11:40:39 +0200

    implemented feature_x

--- test.ps1
+++ test.ps1
@@ -1,3 +1,4 @@
-Get-ChildItem $PSScriptRoot -Filter *.ps1 | % {
+Get-ChildItem -Path $PSScriptRoot -Filter *.ps1 | ForEach-Object -Process {
     Remove-Item -Path $_.FullName
+    Write-Host "Removed $($_.Name)"
 }
